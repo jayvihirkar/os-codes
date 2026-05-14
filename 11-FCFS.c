df4cbf8f2aff3dea77940938a98c91d64a30e23b
Jay Vihirkar <jayvihirkar7@gmail.com>
2026-05-14 10:09:18 +0530

    update again

--- 11-FCFS.c
+++ 11-FCFS.c
@@ -1,33 +1,62 @@
-// FCFS (First Come First Serve) CPU Scheduling
 #include <stdio.h>
 
 int main() {
-    int n, i;
-    
+    int n, i, j;
+
     printf("Enter number of processes: ");
     scanf("%d", &n);
 
-    int bt[n], wt[n], tat[n];
     char p[n][10];
+    int at[n], bt[n], wt[n], tat[n];
 
-    // Input process names and burst times
+    // Input
     for (i = 0; i < n; i++) {
-        printf("Enter Process Name and Burst Time for Process %d: ", i + 1);
-        scanf("%s %d", p[i], &bt[i]);
+        printf("Enter Process Name, Arrival Time and Burst Time: ");
+        scanf("%s %d %d", p[i], &at[i], &bt[i]);
     }
 
-    // FCFS Scheduling
-    wt[0] = 0;               // Waiting time of first process is 0
-    tat[0] = bt[0];          // Turnaround time = Burst Time
+    // Sort by Arrival Time (FCFS rule)
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (at[j] > at[j + 1]) {
+                // Swap arrival time
+                int temp = at[j];
+                at[j] = at[j + 1];
+                at[j + 1] = temp;
 
-    for (i = 1; i < n; i++) {
-        wt[i] = wt[i - 1] + bt[i - 1];
+                // Swap burst time
+                temp = bt[j];
+                bt[j] = bt[j + 1];
+                bt[j + 1] = temp;
+
+                // Swap process name
+                char tempName[10];
+                int k;
+                for (k = 0; k < 10; k++) {
+                    tempName[k] = p[j][k];
+                    p[j][k] = p[j + 1][k];
+                    p[j + 1][k] = tempName[k];
+                }
+            }
+        }
+    }
+
+    // Calculate Waiting Time and Turnaround Time
+    int current_time = 0;
+
+    for (i = 0; i < n; i++) {
+        // CPU may be idle until process arrives
+        if (current_time < at[i])
+            current_time = at[i];
+
+        wt[i] = current_time - at[i];
         tat[i] = wt[i] + bt[i];
+
+        current_time += bt[i];
     }
 
     // Calculate averages
     float avg_wt = 0, avg_tat = 0;
-
     for (i = 0; i < n; i++) {
         avg_wt += wt[i];
         avg_tat += tat[i];
@@ -36,20 +65,15 @@ int main() {
     avg_wt /= n;
     avg_tat /= n;
 
-    // Display results
-    printf("\nFCFS Scheduling Result:\n");
-    printf("-------------------------------------------------\n");
-    printf("Process\tBurst Time\tWaiting Time\tTurnaround Time\n");
-    printf("-------------------------------------------------\n");
-
+    // Output
+    printf("\nProcess\tAT\tBT\tWT\tTAT\n");
     for (i = 0; i < n; i++) {
-        printf("%s\t%d\t\t%d\t\t%d\n",
-               p[i], bt[i], wt[i], tat[i]);
+        printf("%s\t%d\t%d\t%d\t%d\n",
+               p[i], at[i], bt[i], wt[i], tat[i]);
     }
 
-    printf("-------------------------------------------------\n");
-    printf("Average Waiting Time    = %.2f\n", avg_wt);
-    printf("Average Turnaround Time = %.2f\n", avg_tat);
+    printf("\nAverage Waiting Time = %.2f", avg_wt);
+    printf("\nAverage Turnaround Time = %.2f\n", avg_tat);
 
     return 0;
 }
